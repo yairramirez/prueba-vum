cd3a3c316ee03ade843f8b50bd7daf7d3c0e913b
yairramirez <34115079+yairramirez@users.noreply.github.com>
2021-10-28 22:28:19 -0500

    28/10/2021 YR
    - Primer conexión con firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp( functions.config().firebase );
 
 exports.add = functions.https.onRequest((req, res) => {
-  res.send('HOLA MUNDO');
+  admin.firestore().collection('items').add({
+    text: req.query.text,
+  }).then( (resp) => {
+    res.send(`Completado ${ resp }`);
+  }).catch( (err) => {
+    res.send(`Hubo un error ${ err }`);
+  });
 });
